87b537bf9c9bc3e709ef66d251106c3b4e7003a5
yeshealer <yeshealer@gmail.com>
2023-01-26 17:41:11 +0900

    update

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    OPENAI_API_KEY: 'x4a_tn$p%n$m2b_n3n4q$z3q$o#s4r#l%b3d3w2w2w2b5h3g2um2y4s4v4n$j3h%l%p%f3m3m#w%p3l$j%n3w4s4r4h_m$w$w#a4g%',
     MY_OVERVIEW: "Hello everyone! I'm Adan Cui, a Senior Software Engineer with over 5 years of experience. I was born and raised in Miami and graduated from the University of Helsinki in 2018. If you want to know more about me or have any questions, please don't hesitate to ask.",
     DEFAULT_CHAT_PROMPT: "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\n"
   }
--- pages/api/openai-chat.ts
+++ pages/api/openai-chat.ts
@@ -1,9 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { Configuration, OpenAIApi } from "openai";
+import { decode } from 'string-encode-decode'
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+    apiKey: decode(process.env.OPENAI_API_KEY),
     organization: 'yeshealer'
 });
 const openAi = new OpenAIApi(configuration);
